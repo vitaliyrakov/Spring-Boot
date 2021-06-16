ce1a3cfda987c0deece773326b4101b93e31b0fe
VitaliyR <vitaliy.rakov@gmail.com>
2021-06-16 20:55:47 +0300

    hw5 ver_4

--- src/main/java/com/example/demo/config/HibernateXMLConf.java
+++ src/main/java/com/example/demo/config/HibernateXMLConf.java
@@ -1,24 +1,24 @@
-package com.example.demo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-
-@Profile("xml")
-@Configuration
-public class HibernateXMLConf {
-    @Bean
-    public EntityManagerFactory entityManagerFactory() {
-        return new org.hibernate.cfg.Configuration()
-                .configure("hibernate.cfg.xml")
-                .buildSessionFactory();
-    }
-
-    @Bean
-    public EntityManager entityManager() {
-        return entityManagerFactory().createEntityManager();
-    }
-}
+//package com.example.demo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.EntityManagerFactory;
+//
+//@Profile("xml")
+//@Configuration
+//public class HibernateXMLConf {
+//    @Bean
+//    public EntityManagerFactory entityManagerFactory() {
+//        return new org.hibernate.cfg.Configuration()
+//                .configure("hibernate.cfg.xml")
+//                .buildSessionFactory();
+//    }
+//
+//    @Bean
+//    public EntityManager entityManager() {
+//        return entityManagerFactory().createEntityManager();
+//    }
+//}
